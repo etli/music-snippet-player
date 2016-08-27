3695935a6bf6c80a5f8983d45715bdf8798788c5
Elizabeth Li <etli2013@gmail.com>
2016-08-26 22:03:37 -0700

    add loading state, don’t preload everything
    Former-commit-id: 2085109278522eb9c47e05f6efb6c4aa862b22af

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,28 @@
+// 'use strict';
+
 $('.song button').on('mouseenter', function() {
-  playTrack($(this));
+  loadTrack($(this));
 }).on('mouseleave', function() {
   resetTrack($(this));
 });
 
 function loadTrack($button) { 
-  // $button.removeClass().addClass('loading');
-  // $('icon', $button).removeClass().addClass('icon-spinner animate-spin');
-  // $('label', $button).text('Loading...');
-  
-  // console.log('Loading...');
-  
-  // audio.addEventListener('canplay', function() {
-  //   console.log('Ready to play: ' + track);
+  var audio = $button.next('audio')[0];
+
+  if (!$(audio).attr('data-canplay')) {
+    $button.removeClass().addClass('loading');
+    $('icon', $button).removeClass().addClass('icon-spinner animate-spin');
+    $('label', $button).text('Loading...');
+
+    audio.load();
+    audio.addEventListener('canplay', function() {
+      $(audio).attr('data-canplay', 'canplay');
+      playTrack($button);
+    });
+  } else {
     playTrack($button);
-  // });
-}
+  }
+};
 
 function playTrack($button) {
   $button.removeClass().addClass('playing');
@@ -23,9 +30,8 @@ function playTrack($button) {
   $('label', $button).text('Playing Preview');
 
   var audio = $button.next('audio')[0];
-  // audio.load();
   fadeInTrack(audio);
-}
+};
 
 function resetTrack($button) {
   $button.removeClass().addClass('default');
@@ -34,16 +40,16 @@ function resetTrack($button) {
 
   var audio = $button.next('audio')[0];
   fadeOutTrack(audio);
-}
+};
 
 function fadeInTrack(audio) {
   audio.volume = 0;
   audio.play();
   $(audio).stop().animate({volume: 1}, 1000);
-}
+};
 
 function fadeOutTrack(audio) {
   $(audio).stop().animate({volume: 0}, 1000, function() {
     audio.pause();
   });
-}
+};
